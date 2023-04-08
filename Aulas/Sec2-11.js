3d16058321acffd9e8515af3e135a137fb6cdc50
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-04-08 19:59:53 -0300

    aulas

---
+++ Aulas/Sec2-11.js
@@ -0,0 +1,8 @@
+// Não sera executado pq é um comentário - esse é um comentário de linha
+console.log('Olá mundo!');
+console.log(123);
+// vai ser ignorado
+/*
+um bloco de
+linha de comentário
+*/
---
+++ Aulas/sec2-12.js
@@ -0,0 +1,4 @@
+console.log('IU');
+
+console.log("Olá");
+console.log("Esse trecho irá aparecer no console. ")
