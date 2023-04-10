e106942c5a5ccfbaead56e5ea3211e139a84120c
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-04-09 22:28:26 -0300

    novas aulas

---
+++ Aulas/sec2-21/script.js
@@ -0,0 +1,35 @@
+
+// window.alert('Xuxa'); // undefined
+// alert('Baby do baby')// undefined
+// confirm('você é um et?'); // true or false
+// prompt('qual é o seu nome?') // vc pode digitar
+
+// const confirma = confirm('Realmente deseja apagar?');
+// let num1 = prompt('Digite um número.');
+// let num2 = prompt('Digite outro numero'); // tudo retornado pelo prompt vem em string
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2);
+// num3 = num1 + num2
+
+
+// let num1 = prompt('Digite um número');
+// let num2 = prompt('Digite outro valor');
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2);
+// let resultado = num1 + num2;
+// alert(`O resultado é  ${resultado}`);
+
+// let num3 = prompt('Digite um valor:')
+// let num4 = prompt('Digite outro valor:')
+// num3 = parseFloat(num3);
+// num4 = parseFloat(num4);
+// let Resultado4 = num3 * num4;
+// alert(`O resultado é ${Resultado4}`);
+
+let num5 = prompt('Digite um valor:')
+let num6 = prompt('Digite outro número:')
+num5 = Number(num5);
+num6 = Number(num6);
+let resultado5 = num5 ** num6;
+alert(`O resultado é ${resultado5}`);
+alert(`O resultado é ${num5 ** num6}`); // pode se usar assim, porém não é bom.
