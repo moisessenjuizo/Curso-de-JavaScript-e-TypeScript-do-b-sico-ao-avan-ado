44fbbe2d1194a0a66511aa79a5d91ab3a27ad609
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-04-16 03:59:10 -0300

    52

---
+++ Aulas/Aulas-Secao3/for-classico-52.js
@@ -0,0 +1,18 @@
+// i = index
+// for (let i = 0; i <= 500; i += 10){ //i++ aumenta de 1 em 1
+//     console.log(`Linha ${i}`);
+// }
+// for (let i = 500; i >= 400; i -= 10){ //i-- tira
+//     console.log(`Linha ${i}`);
+// }
+// for (let i = 0; i <= 10; i++){
+//     const par  = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i, par);
+// }
+
+const frutas = ['maça', 'pera', 'uva', 'joao', 'gay', 'pereira', 'banana'];
+
+for (let i = 0; i < frutas.length; i++){
+    //console.log(i);
+    console.log(`Índice ${i}` ,frutas[i]);
+}
