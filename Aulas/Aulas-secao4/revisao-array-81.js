35211d1e8dbde0dbfa454b93fefebd17461a54a7
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-06 13:00:42 -0300

    array

---
+++ Aulas/Aulas-secao4/revisao-array-81.js
@@ -0,0 +1,28 @@
+// const nomes = ["Edu", "Maria", "Joana", "Wallace", "Rosana"]; // array literal
+//const nomes = new Array("Edu", "Maria", "Joana");
+// const novo1 = [...nomes];
+
+// console.log(nomes);
+// nomes[2] = "João";
+// console.log(nomes);
+// delete nomes[2];
+// console.log(nomes);
+// const novo = nomes;
+// const removido = novo.pop(); // pop remove
+// console.log(novo);
+// console.log(novo1);
+
+// console.log(nomes.length);
+// console.log(removido);
+
+// nomes.push("Joao"); // adiciona
+// nomes.unshift("Joao"); // foi pro indice 0
+// nomes.unshift("Wallace"); // foi pro indice 1
+
+// const novo = nomes.slice(2, -1);
+// console.log(novo);
+
+// const nomes = nome.split(","); // virou uma array
+const nomes = ["Luiz", "Otávio"];
+const nome = nomes.join(" ");
+console.log(nome);
